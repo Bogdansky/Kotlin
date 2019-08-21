c6b4ad3fe8472f29783170e141decfc26c5e2ac0
Bogdan <bogdya2@gmail.com>
2019-08-21 11:36:30 +0300

    Ariphmetical operations

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,16 @@
 fun main(args: Array<String>){
     var int = 5;
+    val inttoo = 4;
     int = int shr 2;
     println("$int");
     int = int shl 2;
     println("$int");
     int = int.inv();
     println(int)
+    var b = (int or inttoo > 1) or false;
+    var c = (int or inttoo < 1) and true;
+    var d = b xor c;
+    println(b);
+    println(c);
+    println(d);
 }
