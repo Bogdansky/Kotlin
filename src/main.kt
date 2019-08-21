ad40f46218c6a5a4d5a7f3a9a7fe650f19245323
Bogdan <bogdya2@gmail.com>
2019-08-21 12:09:08 +0300

    condition constructions

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime
+
 fun main(args: Array<String>){
     var int = 5;
     val inttoo = 4;
@@ -18,10 +20,30 @@ fun main(args: Array<String>){
     println(result);
     val x = 3;
     println("Included $x in range 1..10: " + checkIn(x));
+    println("Which number 9: "+returningValueByIfElse(9));
+    println(beginMiddleEnd(LocalDateTime.now().dayOfMonth));
 }
 
 fun checkIn(variable: Int): Boolean{
     val lowerBound = 1;
     val topBound = 10;
     return variable in lowerBound..topBound;
+}
+
+fun returningValueByIfElse(variable: Int): String{
+    return if (variable !in -9..9){
+        "Composite"
+    } else {
+        "Simple"
+    }
+}
+
+fun beginMiddleEnd(day: Int): String {
+    println("Now is $day");
+    val part = when (day){
+        in 1..10 -> "Begin"
+        in 11..20 -> "Middle"
+        else -> "End"
+    }
+    return "$part of month";
 }
